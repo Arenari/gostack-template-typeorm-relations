fbd2eced428b00030c5d450246394eb11ae78ec7
Hugo Arenari <hugoarenari@gmail.com>
2021-02-02 22:01:20 -0300

    repositories with typeorm

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -1,4 +1,4 @@
-import { getRepository, Repository, In } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
@@ -7,6 +7,10 @@ import Product from '../entities/Product';
 
 interface IFindProducts {
   id: string;
+}
+
+interface IQuantityMap {
+  [key: string]: number;
 }
 
 class ProductsRepository implements IProductsRepository {
@@ -21,21 +25,51 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({ where: { name } });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productsWithIds = await this.ormRepository.findByIds(
+      products.map(product => product.id),
+    );
+
+    return productsWithIds;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productsWithIds = await this.ormRepository.findByIds(
+      products.map(product => product.id),
+    );
+
+    const quantityPerProduct = products.reduce<IQuantityMap>(
+      (obj, product) => ({ ...obj, [product.id]: product.quantity }),
+      {},
+    );
+
+    const updatedProducts = productsWithIds.map(product => ({
+      ...product,
+      quantity: quantityPerProduct[product.id],
+    }));
+
+    await this.ormRepository.save(updatedProducts);
+
+    return updatedProducts;
   }
 }
 
