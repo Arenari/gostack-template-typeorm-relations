69b806266dde3a9ff5b564bd0a08b9943fe686c1
Hugo Arenari <hugoarenari@gmail.com>
2021-02-02 22:01:25 -0300

    create order service

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -17,16 +17,62 @@ interface IRequest {
   products: IProduct[];
 }
 
+interface IQuantityMap {
+  [key: string]: number;
+}
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+    if (!customer) {
+      throw new AppError('Invalid customer');
+    }
+    const productsList = await this.productsRepository.findAllById(products);
+    if (productsList.length !== products.length) {
+      throw new AppError('Invalid product');
+    }
+
+    const quantityPerProduct = products.reduce<IQuantityMap>(
+      (obj, product) => ({ ...obj, [product.id]: product.quantity }),
+      {},
+    );
+
+    const productWithInsufficientQuantity = productsList.findIndex(
+      product => product.quantity < quantityPerProduct[product.id],
+    );
+
+    if (productWithInsufficientQuantity !== -1) {
+      throw new AppError('Insufficient stock quantity');
+    }
+
+    const productsWithQuantitySubtracted = productsList.map(product => ({
+      id: product.id,
+      quantity: product.quantity - quantityPerProduct[product.id],
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: productsList.map(product => ({
+        product_id: product.id,
+        price: product.price,
+        quantity: quantityPerProduct[product.id],
+      })),
+    });
+
+    await this.productsRepository.updateQuantity(
+      productsWithQuantitySubtracted,
+    );
+
+    return order;
   }
 }
 
